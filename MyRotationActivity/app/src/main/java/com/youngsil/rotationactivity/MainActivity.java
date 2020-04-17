37a24dc17a7f01dc9809981cd965324ac577f13f
youngsil <kys4548@gmail.com>
2020-04-17 19:02:03 +0900

    27session ~ing

--- MyRotationActivity/app/src/main/java/com/youngsil/rotationactivity/MainActivity.java
+++ MyRotationActivity/app/src/main/java/com/youngsil/rotationactivity/MainActivity.java
@@ -26,5 +26,6 @@ public class MainActivity extends AppCompatActivity {
 
     public void showToast(String data) {
         Toast.makeText(this, data, Toast.LENGTH_SHORT).show();
+        //
     }
 }
