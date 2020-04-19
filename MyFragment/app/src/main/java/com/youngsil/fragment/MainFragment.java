6d9a430c43f1f55fce0f6d664c8303eb0b931875
youngsil <kys4548@gmail.com>
2020-04-19 13:24:46 +0900

    Fragment Practice

---
+++ MyFragment/app/src/main/java/com/youngsil/fragment/MainFragment.java
@@ -0,0 +1,28 @@
+package com.youngsil.fragment;
+
+import android.os.Bundle;
+import android.widget.Button;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.fragment.app.FragmentActivity;
+
+public class MainFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_main, container, false);
+
+        Button button = rootView.findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MainActivity mainActivity = (MainActivity) getActivity();
+                mainActivity.onFragmentChanged(1);
+            }
+        });
+
+        return rootView;
+    }
+}
