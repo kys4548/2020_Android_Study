e6373a72196edeaeb836f7ce1beedfdf05f66515
youngsil <kys4548@gmail.com>
2020-04-19 11:37:14 +0900

    parcelable

---
+++ MyParcelable/app/src/main/java/com/youngsil/parcelable/MainActivity.java
@@ -0,0 +1,27 @@
+package com.youngsil.parcelable;
+
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MenuActivity.class);
+                SimpleData data = new SimpleData(200, "OK");
+                intent.putExtra("data", data);
+                startActivity(intent);
+            }
+        });
+    }
+}
